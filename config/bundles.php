fe928bf3ea97263e39a6d9bd54c3a1c78cf8790c
Tac Tacelosky <tacman@gmail.com>
2019-12-18 22:36:27 -0500

    add sentry

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Survos\LandingBundle\SurvosLandingBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
