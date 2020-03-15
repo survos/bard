014e0dabbc35cee4e85bbd2f3b6a37834864d5a8
Tac Tacelosky <tacman@gmail.com>
2020-03-15 08:52:05 -0400

    cleanup menus

--- src/Menu/MenuBuilder.php
+++ src/Menu/MenuBuilder.php
@@ -18,11 +18,12 @@ $menu->addChild('survos_landing', ['route' => 'app_homepage'])->setAttribute('ic
 $menu->addChild('survos_landing_credits', ['route' => 'survos_landing_credits'])->setAttribute('icon', 'fas fa-trophy');
 $menu->addChild('app_typography', ['route' => 'app_typography'])->setAttribute('icon', 'fab fa-bootstrap');
 
-$menu->addChild('work_index', ['route' => 'work_index'])->setAttribute('icon', 'fas fa-theater-masks');
-$menu->addChild('test_rdf', ['route' => 'test_rdf'])->setAttribute('icon', 'fas fa-sync');
+$menu->addChild('work_index.title', ['route' => 'work_index'])->setAttribute('icon', 'fas fa-theater-masks');
+
+// $menu->addChild('test_rdf', ['route' => 'test_rdf'])->setAttribute('icon', 'fas fa-sync');
 $menu->addChild('datatable', ['route' => 'work_datatable'])->setAttribute('icon', 'fas fa-table');
 $menu->addChild('api', ['route' => 'api_entrypoint'])->setAttribute('icon', 'fas fa-exchange-alt');
-$menu->addChild('easyadmin', ['route' => 'easyadmin'])->setAttribute('icon', 'fas fa-gears');
+$menu->addChild('easyadmin', ['route' => 'easyadmin'])->setAttribute('icon', 'fas fa-database');
 
 
 
