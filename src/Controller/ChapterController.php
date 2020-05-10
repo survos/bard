1f69aa8aacc5e239782d278c0e053d176e6bff22
Tac Tacelosky <tacman@gmail.com>
2020-05-10 11:15:57 -0400

    add chapter crud (from SurvosMakeCrud)

---
+++ src/Controller/ChapterController.php
@@ -0,0 +1,99 @@
+<?php
+
+
+// uses Survos Param Converter, from the UniqueIdentifiers method of the entity.
+
+namespace App\Controller;
+
+use App\Entity\Chapter;
+use App\Form\ChapterType;
+use App\Repository\ChapterRepository;
+// use Symfony\Bundle\FrameworkBundle\Controller\BaseController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Survos\LandingBundle\Controller\BaseController;
+
+/**
+ * @Route("/chapter")
+ */
+class ChapterController extends BaseController
+{
+    /**
+     * @Route("/", name="chapter_index", methods={"GET"})
+     */
+    public function index(ChapterRepository $chapterRepository): Response
+    {
+        return $this->render('chapter/index.html.twig', [
+            'chapters' => $chapterRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="chapter_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $chapter = new Chapter();
+        $form = $this->createForm(ChapterType::class, $chapter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($chapter);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('chapter_index');
+        }
+
+        return $this->render('chapter/new.html.twig', [
+            'chapter' => $chapter,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{chapterId}", name="chapter_show", methods={"GET"})
+     */
+    public function show(Chapter $chapter): Response
+    {
+        return $this->render('chapter/show.html.twig', [
+            'chapter' => $chapter,
+        ]);
+    }
+
+    /**
+     * @Route("/{chapterId}/edit", name="chapter_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Chapter $chapter): Response
+    {
+        $form = $this->createForm(ChapterType::class, $chapter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('chapter_index');
+        }
+
+        return $this->render('chapter/edit.html.twig', [
+            'chapter' => $chapter,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{chapterId}", name="chapter_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Chapter $chapter): Response
+    { 
+        // hard-coded to getId, should be get parameter of uniqueIdentifiers()
+        if ($this->isCsrfTokenValid('delete'.$chapter->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($chapter);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('chapter_index');
+    }
+}
