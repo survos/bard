0df3b84d280cd884bab664c61c93ff93241c60a5
Tac Tacelosky <tacman@gmail.com>
2021-03-02 09:19:11 -0500

    allow php7

--- src/EventSubscriber/SidebarMenuSubscriber.php
+++ src/EventSubscriber/SidebarMenuSubscriber.php
@@ -38,7 +38,9 @@ class SidebarMenuSubscriber extends  BaseMenuSubscriber implements EventSubscrib
 
     private function isDev(): bool
     {
-        return $this->getParameterBag()?->get('kernel.environment') == 'dev';
+        // php8
+//        return $this->getParameterBag()?->get('kernel.environment') == 'dev';
+        return $this->getParameterBag()->get('kernel.environment') == 'dev';
     }
 
     public function onKnpMenuEvent(KnpMenuEvent $event)
