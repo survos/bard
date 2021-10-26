59e03c2ab6d70fdd5c6cec53bbda8ab585a96201
Tac Tacelosky <tacman@gmail.com>
2021-10-26 08:10:15 -0400

    add initial version of tabler

--- config/bundles.php
+++ config/bundles.php
@@ -25,4 +25,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    KevinPapst\TablerBundle\TablerBundle::class => ['all' => true],
 ];
